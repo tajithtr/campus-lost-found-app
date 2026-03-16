2a38f84be56a0fdf8b8eb0911246b5f27a1c89ed
Tajith Rashidha <tajithrashidha@gmail.com>
2026-03-16 22:39:06 +0530

    setup app entry point and navigation routes
    - Updated main.dart to initialize Firebase and start the application
    - Added app_routes.dart for centralized navigation management

--- campus_lost_found_app/lib/main.dart
+++ campus_lost_found_app/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -16,68 +15,21 @@ class LostFoundApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Campus Lost & Found',
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomeScreen(),
+      home: SplashScreen(),
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  String message = "Checking Firebase connection...";
-
-  @override
-  void initState() {
-    super.initState();
-    testFirestore();
-  }
-
-  Future<void> testFirestore() async {
-    try {
-      await FirebaseFirestore.instance.collection('test').doc('connection').set(
-        {'status': 'connected'},
-      );
-
-      var doc = await FirebaseFirestore.instance
-          .collection('test')
-          .doc('connection')
-          .get();
-
-      if (doc.exists && doc.data()?['status'] == 'connected') {
-        setState(() {
-          message = "Firebase is fully connected!";
-        });
-      } else {
-        setState(() {
-          message = "Firebase read/write test failed.";
-        });
-      }
-    } catch (e) {
-      setState(() {
-        message = "Firebase error: $e";
-      });
-    }
-  }
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Campus Lost & Found')),
+    return const Scaffold(
       body: Center(
-        child: Text(
-          message,
-          style: const TextStyle(fontSize: 18),
-          textAlign: TextAlign.center,
-        ),
+        child: Text("Campus Lost & Found", style: TextStyle(fontSize: 24)),
       ),
     );
   }
