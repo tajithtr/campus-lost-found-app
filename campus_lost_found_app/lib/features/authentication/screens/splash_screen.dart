3c555a1e3ad548c4eb064df1537929d49d879880
Bhagya Abeykoon <152451688+Bhagyaabeykoon@users.noreply.github.com>
2026-03-20 13:30:03 +0530

    splash screen UI
    Added splash screen with bold text and image

---
+++ campus_lost_found_app/lib/features/authentication/screens/splash_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          // 🔹 Full Screen Image
+          SizedBox.expand(
+            child: Image.asset(
+              'assets/images/splash.png',
+              fit: BoxFit.contain, // important ✅
+            ),
+          ),
+
+          // 🔹 Tagline Text
+          const Positioned(
+            bottom: 250, // adjust based on your image
+            left: 20,
+            right: 20,
+            child: Text(
+              "Helping lost items find their way back.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 13,
+                color: Color.fromARGB(255, 0, 0, 0),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
