95be24a0cde0b51f8ccf98cf867d8091eeb36b0e
Bhagya Abeykoon <152451688+Bhagyaabeykoon@users.noreply.github.com>
2026-04-02 10:14:13 +0530

    auth-feature: implement secure password reset flow using Firebase
    Developed Forgot Password feature with email-based reset
    Integrated FirebaseAuth sendPasswordResetEmail API
    Added form validation for user email input
    Tested complete flow including email delivery and reset link handling
    Improved authentication UX by enabling account recovery

--- campus_lost_found_app/android/app/build.gradle.kts
+++ campus_lost_found_app/android/app/build.gradle.kts
@@ -1,14 +1,15 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.campus_lost_found_app"
-    compileSdk = flutter.compileSdkVersion
+
+    compileSdk = 36
+
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -17,24 +18,21 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = "17"
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.campus_lost_found_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
+
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -45,12 +43,9 @@ flutter {
 }
 
 dependencies {
-  // Import the Firebase BoM
-  implementation(platform("com.google.firebase:firebase-bom:34.10.0"))
-  implementation("com.google.firebase:firebase-auth")
+    // Firebase BOM
+    implementation(platform("com.google.firebase:firebase-bom:34.10.0"))
 
-
-  // TODO: Add the dependencies for Firebase products you want to use
-  // When using the BoM, don't specify versions in Firebase dependencies
-  // https://firebase.google.com/docs/android/setup#available-libraries
+    // Firebase Auth
+    implementation("com.google.firebase:firebase-auth")
 }
