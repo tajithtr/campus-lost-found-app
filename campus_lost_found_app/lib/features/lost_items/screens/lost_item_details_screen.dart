71a3730db8130790e3e5ccbdc703fc67767110e3
Tajith Rashidha <tajithrashidha@gmail.com>
2026-04-08 19:19:07 +0530

    add back arrow navigation to lost item details page

--- campus_lost_found_app/lib/features/lost_items/screens/lost_item_details_screen.dart
+++ campus_lost_found_app/lib/features/lost_items/screens/lost_item_details_screen.dart
@@ -21,8 +21,11 @@ class _LostItemDetailsScreenState extends State<LostItemDetailsScreen> {
         backgroundColor: Color(0xFF1F3C88),
         centerTitle: true,
         elevation: 0,
-        iconTheme: IconThemeData(color: Colors.white),
-        leading: IconButton(icon: Icon(Icons.arrow_back), onPressed: () {}),
+        iconTheme: const IconThemeData(color: Colors.white),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+        ),
         title: Text("Item Details", style: TextStyle(color: Colors.white)),
       ),
       body: SingleChildScrollView(
